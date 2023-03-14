1c991f08745182a066290e307f3bafa63db4bacf
FeatherRian <1178441170@qq.com>
2023-03-14 20:16:49 +0800

    ✨ 更新旋转视角、放大缩小视角、光照自然旋转

---
+++ Assets/Planet/Script/LightController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public float time = 0;
+    public Transform lightCenter;
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 1 / rotateSpeed)
+        {
+            time -= 1 / rotateSpeed;
+        }
+        lightCenter.eulerAngles = new Vector3(0, 0, -time * rotateSpeed * 360);
+    }
+}
