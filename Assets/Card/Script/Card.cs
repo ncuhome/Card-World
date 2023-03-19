938c0349433d57c9bae62f43ac5f50ccf62c073a
Zhang <2308049967@qq.com>
2023-03-19 15:42:40 +0800

    :rocket: 制作动画

--- Assets/Card/Script/Card.cs
+++ Assets/Card/Script/Card.cs
@@ -2,21 +2,30 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler, IDragHandler, IEndDragHandler
 {
-    public static int cardSizex = 100;
-    public static int cardSizey = 150;
-    public static float firstTime = 0.5f;  //抽卡第一阶段时长
-    public static float secondTime = 1.5f; //抽卡第二阶段时长
+    public static int cardSizex = 100;     //卡牌宽度
 
+    public static int cardSizey = 150;     //卡牌高度
+
+    public static float firstTime = 0.5f;  //抽卡第一阶段时长 变大并展示
+
+    public static float secondTime = 1.5f; //抽卡第二阶段时长 缩小进入卡槽
+
+    public Sequence pointerSequence;
+
+    private bool canBeDrag;
+    [SerializeField] private Canvas cardCanvas;
     private void Start() //卡牌被创建后立即执行
     {
-        Drawcards();
+        Drawcards(); 
     }
     public virtual void BeUse()  //使用卡牌函数
     {
-
+        
     }
     public void Drawcards()
     {
@@ -26,9 +35,40 @@ public class Card : MonoBehaviour
         quence.Join(transform.DOScale(new Vector2(3f, 3f), firstTime));
         quence.Join(transform.DORotate(new Vector2(15, -90), firstTime).From());
         quence.Append(transform.DOPunchPosition(new Vector2(5, 10), 0.2f, 1, 0.1f)); //卡牌震动
-        quence.AppendInterval(0.6f);                                                 //暂停
-        quence.Append(transform.DOLocalMove(CardPack.AddCard(this), secondTime));    //回到卡槽
-        quence.Join(transform.DOScale(new Vector2(1f, 1f), secondTime));
+        quence.AppendInterval(0.6f).OnComplete(() => { CardPack.AddCard(this); transform.DOScale(new Vector2(1f, 1f), secondTime); });   
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.transform.SetAsLastSibling(); //显示在所有卡牌的最上面
+        this.pointerSequence.Append(transform.DOScale(new Vector2(1.5f, 1.5f), 0.1f));  //变大
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.pointerSequence.Append(transform.DOScale(new Vector2(1f, 1f), 0.1f));      //变小
+    }
+
+    public void OnDrag(PointerEventData eventData) //卡牌拖拽效果
+    {
+        this.GetComponent<RectTransform>().anchoredPosition += 
+            eventData.delta / this.transform.parent.transform.parent.GetComponent<Canvas>().scaleFactor;  
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (this.GetComponent<RectTransform>().anchoredPosition.y >= CardPack.cardPackHigh + Card.cardSizey + 50)
+        {
+            canBeDrag = false;
+            CardPack.DeleteCard(this);
+            transform.DOLocalMove(new Vector2(0, 540), 0.5f);
+            transform.DOScale(new Vector2(2f, 2f), 0.5f).
+                OnComplete(() => { this.GetComponent<Image>().DOFade(0, 0.2f).OnComplete(() => { Destroy(this); }); });
+        }
+        else
+        {
+            CardPack.SortCard();
+        }
     }
 
 }
--- Assets/Card/Script/CardPack.cs
+++ Assets/Card/Script/CardPack.cs
@@ -6,23 +6,28 @@ using UnityEngine;
 public class CardPack : MonoBehaviour //卡牌背包
 {
     public int cardMaximums = 10;
+    public static float cardPackHigh;
     public static List<MonoBehaviour> cardPack = new List<MonoBehaviour>();
-    public static Vector2 AddCard(Card newCard)
+    private void Awake()
     {
-        Vector2 endVector;  //新增卡牌最后移动到的位置
-        cardPack.Add(newCard); //向卡包中加入新卡牌
-        for (int i = 0; i < cardPack.Count - 1; i++)
+        cardPackHigh = this.transform.position.y;
+    }
+    public static void AddCard(Card newCard)
+    {
+        cardPack.Add(newCard); //向卡包列表中加入新卡牌
+        SortCard();
+    }
+    public static void DeleteCard(Card card)
+    {
+        cardPack.Remove(card);
+        SortCard();
+    }
+    public static void SortCard()
+    {
+        float first = -Card.cardSizex / 2 * (cardPack.Count - 1);
+        for (int i = 0; i < cardPack.Count; i++)
         {
-            cardPack[i].gameObject.transform.DOLocalMoveX(cardPack[i].gameObject.transform.localPosition.x - Card.cardSizex / 2 , Card.secondTime);
+            cardPack[i].gameObject.transform.DOLocalMove(new Vector2(first + i * Card.cardSizex, 0), Card.secondTime);
         }
-        if (cardPack.Count == 1)
-        {
-            endVector = Vector2.zero;
-        }
-        else
-        {
-            endVector = new Vector2(cardPack[cardPack.Count - 2].gameObject.transform.localPosition.x + Card.cardSizex/2, 0);
-        }
-        return endVector;
     }
 }
