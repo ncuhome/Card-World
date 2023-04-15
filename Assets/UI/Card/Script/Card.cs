5359647b5e234ed8856b13b38180a93d862b49cd
ChenMoil <2308049967@qq.com>
2023-04-15 11:30:33 +0800

    :bug: 修复UIbug

--- Assets/UI/Card/Script/Card.cs
+++ Assets/UI/Card/Script/Card.cs
@@ -110,10 +110,9 @@ public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ID
             else if (this.GetComponent<RectTransform>().anchoredPosition.y >= CardPack.cardPackHigh + Card.cardSizey + 50)
             {
                 canBeDrag = false;
-                CardPack.DeleteCard(this);
                 transform.DOLocalMove(new Vector2(0, 540), 0.5f);
                 transform.DOScale(new Vector2(2f, 2f), 0.5f).
-                    OnComplete(() => { Destroy(this.transform.GetChild(0).gameObject); this.transform.GetChild(1).GetComponent<Image>().DOFade(0, 0.2f).OnComplete(() => { CardPack.DeleteCard(this); }); });
+                    OnComplete(() => { Destroy(this.transform.GetChild(0).gameObject); this.transform.GetChild(1).GetComponent<Image>().DOFade(0, 0.2f).OnComplete(() => { CardPack.DeleteCard(this);Destroy(this.gameObject); }); });
                 BeUse();
             }
             else
@@ -133,16 +132,15 @@ public class AccidentCard : Card
         {
             if (Vector2.Distance(this.transform.position, cardTrash.transform.position) < 150)  //在垃圾桶范围内
             {
-                SignUI.instance.DisplayText("You can't broke it", 1f, Color.red);
+                SignUI.instance.DisplayText("你无法摧毁意外卡", 1f, Color.red);
                 CardPack.SortCard();
             }
             else if (this.GetComponent<RectTransform>().anchoredPosition.y >= CardPack.cardPackHigh + Card.cardSizey + 50)  //在使用
             {
                 this.canBeDrag = false;
-                CardPack.DeleteCard(this);
                 transform.DOLocalMove(new Vector2(0, 540), 0.5f);
                 transform.DOScale(new Vector2(2f, 2f), 0.5f).
-                    OnComplete(() => { this.GetComponent<Image>().DOFade(0, 0.2f).OnComplete(() => { Destroy(this); }); });
+                    OnComplete(() => { Destroy(this.transform.GetChild(0).gameObject); this.transform.GetChild(1).GetComponent<Image>().DOFade(0, 0.2f).OnComplete(() => { CardPack.DeleteCard(this); Destroy(this.gameObject); }); });
                 BeUse();
             }
             else
